5de8d7aba71ba8e373ce02f6bd0815a6d162b786
Hai Le Hong <20485451+hailehong95@users.noreply.github.com>
2023-06-20 22:41:36 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   blog: {
     title: "DFIR Blog",
     description: "Welcome to DFIR blog!",
-    theme: "auto", // ['light', 'dark', 'auto']
+    theme: "light", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
